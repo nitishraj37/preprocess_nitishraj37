7360f6ff2fd5776a8c411707a596a0cb54c9f2ae
Nitish Raj <nitish.raj37@gmail.com>
2022-02-22 14:45:38 +0530

    Python package for Sentiment Analysis

---
+++ README.md
@@ -0,0 +1,3 @@
+#Preprocessing Text Python Package
+
+This Python Package is created by Nitish Raj
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md','r') as file:
+	long_desciption = file.read()
+
+
+setuptools.setup(
+	name = 'preprocess_nitishraj37',   # this should be unique
+	version = '0.0.1',
+	author = 'Nitish Raj',
+	author_email = 'bantyy354@gmail.com',
+	description = 'This is preprocessing package',
+	long_desciption = 'long_desciption',
+	long_desciption_content_type = 'text/markdown',
+	packages = setuptools.find_packages(),
+	classifiers = [
+	'Programming Language :: Python :: 3',
+	'License :: OSI Aproved :: MIT License',
+	"Operating System :: OS Independent"],
+	python_requires = '>=3.5'
+	)
